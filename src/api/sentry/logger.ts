8bf6e7fece8421ecef8ed56c7d502d4ee4c39e51
JPeer264 <jan.peer@sentry.io>
2026-03-02 15:08:45 +0100

    feat: split up sentry parts

---
+++ src/api/sentry/logger.ts
@@ -0,0 +1,18 @@
+import * as Sentry from "@sentry/node";
+import { Logger, LogLevel } from "effect";
+
+export const SentryLogger = Logger.make(({ logLevel, message }) => {
+  const msg = typeof message === "string" ? message : JSON.stringify(message);
+
+  if (LogLevel.greaterThanEqual(logLevel, LogLevel.Error)) {
+    Sentry.logger.error(msg);
+  } else if (LogLevel.greaterThanEqual(logLevel, LogLevel.Warning)) {
+    Sentry.logger.warn(msg);
+  } else if (LogLevel.greaterThanEqual(logLevel, LogLevel.Info)) {
+    Sentry.logger.info(msg);
+  } else if (LogLevel.greaterThanEqual(logLevel, LogLevel.Debug)) {
+    Sentry.logger.debug(msg);
+  } else {
+    Sentry.logger.trace(msg);
+  }
+});
