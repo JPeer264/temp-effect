7dd25054e27cb8b86638f5f9621eb0605fae3322
JPeer264 <jan.peer@sentry.io>
2026-03-02 11:01:50 +0100

    feat: add some ideas

--- src/api/main.ts
+++ src/api/main.ts
@@ -4,14 +4,6 @@ import { HttpLive } from "./Http.js"
 import { SentryLive } from "./Sentry.js"
 import * as Sentry from "@sentry/node"
 
-const client = Sentry.init({
-  dsn: "https://0299849066e3bcf4626b214897b6c17f@o447951.ingest.us.sentry.io/4510555608449024",
-  environment: process.env.NODE_ENV ?? "development",
-  tracesSampleRate: 1.0,
-  debug: true,
-  enableLogs: true,
-})
-
-const MainLive = HttpLive.pipe(Layer.provideMerge(SentryLive(client)))
+const MainLive = HttpLive.pipe(Layer.provideMerge(SentryLive(Sentry.getClient())))
 
 MainLive.pipe(Layer.launch, NodeRuntime.runMain)
